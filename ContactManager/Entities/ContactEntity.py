758a54c674ef9d0a59ada2e95d69f93d43c292b6
Siddhant Jain <siddhant780@gmail.com>
2021-11-22 11:03:16 +0530

    added contact class and modified design

---
+++ ContactManager/Entities/ContactEntity.py
@@ -0,0 +1,19 @@
+class ContactEntity:
+    first_name = ""
+    last_name = ""
+    country_code = ""
+    phonenumber = 1111111111
+    email_address = ""
+    facebook_id = ""
+    twitter_id = ""
+    website = ""
+
+    #will be address class objects not strings (to be created by Anusha)
+    home_address = ""
+    office_address = ""
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "This is contact for {}, with contact number {}".format(self.first_name + " " + self.last_name, self.phonenumber)
